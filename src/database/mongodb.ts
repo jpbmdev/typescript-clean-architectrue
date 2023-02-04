e07cd8009f15d8217be98812bcd73c29bcc608b0
jpbmdev <jpbmdev@gmail.com>
2023-02-04 14:22:26 -0500

    mongodb db name fix

--- src/database/mongodb.ts
+++ src/database/mongodb.ts
@@ -1,9 +1,10 @@
 import { connect } from "mongoose";
 
 const DB_URL = `${process.env.DB_URL}`;
+const DB_NAME = `${process.env.DB_NAME}`;
 
 const mongoDBInit = async () => {
-  await connect(DB_URL);
+  await connect(DB_URL, { dbName: DB_NAME });
   console.log("Mongodb database connected");
 };
 
